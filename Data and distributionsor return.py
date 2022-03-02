b1b8b58ecb79243ff8da9b6ea9e141abef27531f
gmadurov <gmadurov@gmail.com>
2022-03-02 16:30:57 +0100

    wednesday afternoon

--- Data and distributionsor return.py
+++ Data and distributionsor return.py
@@ -1,3 +1,11 @@
 import numpy as np
 import pandas as pd
-import 
+import matplotlib.pyplot as plt
+
+DF_AMZN = pd.read_csv('AMZN.csv', delimiter=',')
+
+AR_AMZN = np.array(DF_AMZN)
+
+for i, val in enumerate(DF_AMZN.Open):
+    print(DF_AMZN.Open.iloc[i])
+print(DF_AMZN.head())
